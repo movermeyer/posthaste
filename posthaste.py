ffdeafebf991a9aedb90631c967389f8f57b3972
Matt Martz <matt@sivel.net>
2013-09-26 11:22:54 -0500

    Bump version and update url to reflect new github home

--- posthaste.py
+++ posthaste.py
@@ -30,7 +30,7 @@ import functools
 import time
 import threading
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 def handle_args():
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='posthaste',
-    version='0.2.0',
+    version='0.2.1',
     description=('Gevent-based, multithreaded tool for interacting with '
                  'OpenStack Swift and Rackspace Cloud Files'),
     long_description=open('README.rst').read(),
     keywords='rackspace openstack cloud cloudfiles swift',
     author='Matt Martz',
     author_email='matt@sivel.net',
-    url='https://github.com/sivel/posthaste',
+    url='https://github.com/rackerlabs/posthaste',
     license='Apache License, Version 2.0',
     py_modules=['posthaste'],
     install_requires=[
